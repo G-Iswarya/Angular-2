5c63b4feb319959aea7cb1dea2c5931d641c03a1
G-Iswarya <141030852+G-Iswarya@users.noreply.github.com>
2023-10-19 21:08:56 +0530

    Add files via upload

---
+++ src/app/connected/connected.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-connected',
+  templateUrl: './connected.component.html',
+  styleUrls: ['./connected.component.css']
+})
+export class ConnectedComponent {
+showrouter:boolean=false
+showusage:boolean=false
+data:String
+showrouterdetails()
+{
+  this.showrouter = !this.showrouter
+}
+showusagedetails()
+{
+  this.showusage=!this.showusage
+}
+}
